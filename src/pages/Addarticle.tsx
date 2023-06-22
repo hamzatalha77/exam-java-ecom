f01ee87086b3a3a746c023e5963f3d1da2b51500
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-22 11:37:20 +0100

    go

---
+++ src/pages/Addarticle.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const Addarticle = () => {
+  return (
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+          Register
+        </h1>
+        <form className="mt-6">
+          <div className="mb-2">
+            <label className="block text-sm font-semibold text-gray-800">
+              Nom
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-semibold text-gray-800">
+              Prenom
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-semibold text-gray-800">
+              Username
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-semibold text-gray-800">
+              Adresse mail
+            </label>
+            <input
+              type="email"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-semibold text-gray-800">
+              ville
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-semibold text-gray-800">
+              Tél
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Addarticle
