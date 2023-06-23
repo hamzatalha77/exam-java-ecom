54b41bbb2092fa7ed4a42668b2ad461c3c7e42b7
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-23 12:59:34 +0100

    commit

--- src/pages/CartScreen.tsx
+++ src/pages/CartScreen.tsx
@@ -4,6 +4,7 @@ import { FaTrash } from 'react-icons/fa'
 import Message from '../components/Message'
 import '../App.css'
 import { RootState } from '../store'
+import { addToCart } from '../slices/cartSlice'
 
 const CartScreen = () => {
   const navigate = useNavigate()
@@ -11,7 +12,9 @@ const CartScreen = () => {
 
   const cart = useSelector((state: RootState) => state.cart)
   const { cartItems } = cart
-
+  const addToCartHandler = async (article: any, qty: number) => {
+    dispatch(addToCart({ ...article, qty }))
+  }
   return (
     <div>
       <div className="h-full bg-gray-100 pt-20">
@@ -44,7 +47,9 @@ const CartScreen = () => {
                       <select
                         id="quantity"
                         value={item.qty}
-                        onChange={(e) => {}}
+                        onChange={(e) =>
+                          addToCartHandler(item, Number(e.target.value))
+                        }
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       >
                         {Array.from(Array(item.countInStock).keys()).map(
@@ -99,7 +104,10 @@ const CartScreen = () => {
                   <p className="text-sm text-gray-700">including VAT</p>
                 </div>
               </div>
-              <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+              <button
+                className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600"
+                disabled={cartItems.length === 0}
+              >
                 Check out
               </button>
             </div>
