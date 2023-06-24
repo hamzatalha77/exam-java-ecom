97fbf45c52175fa54aa2e1145567a23b18a7fc76
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-24 12:06:04 +0100

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   const [search, setSearch] = useState('')
   return (
     <>
-      <div>
+      <div className="flex justify-center flex-initial">
         <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
           Search
         </label>
@@ -31,7 +31,7 @@ const Home = () => {
             type="search"
             onChange={(e) => setSearch(e.target.value)}
             id="default-search"
-            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Rechercher Votre Article"
           />
         </div>
